b473e852ab39fcf2fd2dfe9d4ba79e9736dd1d4d
Zachary Ferguson <zfergus@nyu.edu>
2023-07-21 13:48:19 -0400

    Remove config.hpp

--- src/config.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// WARNING: Do not modify config.hpp directly. Instead, modify config.hpp.in.
-
-#define FAR_CCD_NAME "FAR_CCD"
-#define FAR_CCD_VER "0.0.1"
-#define FAR_CCD_VER_MAJOR "0"
-#define FAR_CCD_VER_MINOR "0"
-#define FAR_CCD_VER_PATCH "1"
